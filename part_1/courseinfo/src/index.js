db74acf72cbff33b65c49d6930e8d64c29838aa8
Daniel Castillo Guindos <danielcastilloguindos@gmail.com>
2023-01-27 20:10:46 +0100

    Part_1: Step 3

--- part_1/courseinfo/src/index.js
+++ part_1/courseinfo/src/index.js
@@ -5,34 +5,28 @@ import { Header } from './Header'
 import { Total } from './Total'
 
 const App = () => {
-  const course  = {
-    title: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-      }
-    ],
-    getTotalExercises: function () {
-      let total = 0;
-      this.parts.map(part => total += part.exercises );
-      return total;
-    }
+  const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
   }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+  const getTotalExercises = () => {
+    return part1.exercises + part2.exercises + part3.exercises;
+  } 
 
   return (
     <div>
-      <Header title={ course.title } />
-      <Content parts={ course.parts } />
-      <Total value={ course.getTotalExercises() } />
+      <Header title={ course } />
+      <Content parts={ [ part1, part2, part3 ] } />
+      <Total value={ getTotalExercises() } />
     </div>
   )
 }
