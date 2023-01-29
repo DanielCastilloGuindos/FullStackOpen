828a266f03f025e3efe4c68ecc727e485ef2e150
Daniel Castillo Guindos <danielcastilloguindos@gmail.com>
2023-01-29 19:26:55 +0100

    Part_2: infocourse_step_7

--- part_2/courseinfo/src/Components/Course.js
+++ part_2/courseinfo/src/Components/Course.js
@@ -1,13 +1,21 @@
 import { Content } from "./Content";
 import { Header } from "./Header"
+import { Total } from "./Total";
 
 export const Course = ({ course }) => {
 	const { id, name, parts } = course;
+
+	const getTotalExercises = () => {
+		let total = 0;
+		parts.map(part => total += part.exercises);
+		return total;
+	};
 
 	return (
 		<>
 			<Header title={name} />
 			<Content parts={parts} />
+			<Total value={ getTotalExercises() } />
 		</>
 	)
 }
---
+++ part_2/courseinfo/src/Components/Total.js
@@ -0,0 +1,5 @@
+export const Total = ({ value }) => {
+	return (
+		<p><b>Total of { value } exercises</b></p>
+	)
+}
